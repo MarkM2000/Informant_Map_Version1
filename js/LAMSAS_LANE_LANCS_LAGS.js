7d7a442746cbc9df256a832c4f08b47fece89bdc
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-09 12:33:24 -0400

    Updating AccessToken

--- js/LAMSAS_LANE_LANCS_LAGS.js
+++ js/LAMSAS_LANE_LANCS_LAGS.js
@@ -16,7 +16,7 @@ var map = L.map('map', {
 });
 
 // mapbox API parameters
-const accessToken = `pk.eyJ1IjoibWFya20wMCIsImEiOiJjbTFrM2FqZGwwdWFnMmtvY2FlNDFwdWZyIn0.2Z-bz-XInRuh_d5KIW_mrQ`
+const accessToken = `pk.eyJ1IjoibWFya20wMCIsImEiOiJjbTFwOW9ycG0wMXZkMmxxMnduNWlieGJ1In0.VbAhOz1aautpLLn2twMDSQ`
 const yourName = 'markm00'
 const yourMap = 'cm187ejix00lw01pc0smkants'
 
